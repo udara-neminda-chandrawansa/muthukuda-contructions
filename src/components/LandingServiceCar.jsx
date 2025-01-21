88126b27007ce8aa8b44a3b93c04367ad620de04
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-01-21 12:31:04 +0530

    minor fix

--- src/components/LandingServiceCar.jsx
+++ src/components/LandingServiceCar.jsx
@@ -8,28 +8,28 @@ const LSCarousel = () => {
         <div className="w-full h-full bg-white">
           <ul className="flex items-center h-full px-6 text-center text-black max-sm:px-3 justify-evenly max-sm:flex-col">
             <li className="max-sm:flex max-sm:gap-1">
-              <Link href="/services/floor-area-counting">
+              <Link href="/services/">
                 <p className="uppercase max-sm:hidden">Residential</p>
                 <p className="uppercase max-sm:hidden">Construction</p>
               </Link>
             </li>
             <li className="h-1/2 bg-[#aaa] w-[1px] max-sm:h-[1px] max-sm:w-full"></li>
             <li className="max-sm:flex max-sm:gap-1">
-              <Link href="/services/special-loans">
+              <Link href="/services/">
                 <p className="uppercase max-sm:hidden">Commercial</p>
                 <p className="uppercase max-sm:hidden">Construction</p>
               </Link>
             </li>
             <li className="h-1/2 bg-[#aaa] w-[1px] max-sm:h-[1px] max-sm:w-full"></li>
             <li className="max-sm:flex max-sm:gap-1">
-              <Link href="/services/planning-approvals">
+              <Link href="/services/">
                 <p className="uppercase max-sm:hidden">Renovations </p>
                 <p className="uppercase max-sm:hidden">and Remodeling</p>
               </Link>
             </li>
             <li className="h-1/2 bg-[#aaa] w-[1px] max-sm:h-[1px] max-sm:w-full"></li>
             <li className="max-sm:flex max-sm:gap-1">
-              <Link href="/services/landscaping">
+              <Link href="/services/">
                 <p className="uppercase max-sm:hidden">Sustainable </p>
                 <p className="uppercase max-sm:hidden">Construction</p>
               </Link>
